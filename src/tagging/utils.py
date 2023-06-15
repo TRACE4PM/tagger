973064af928c18e2a14828684bcb0261af32d350
nalcime <nannito.alcime@etudiant.univ-lr.fr>
2023-06-15 16:23:38 +0200

    Little change in utils.py

--- src/tagging/utils.py
+++ src/tagging/utils.py
@@ -5,5 +5,6 @@ async def validate_json_content(file_content):
     tags_config = json.loads(file_content)
     tags = []
     for tag_config in tags_config:
-        tags.append(Tag_Rules_Model(**tag_config))
+        #tags.append(Tag_Rules_Model(**tag_config))
+        tags.append(**tag_config)
     return tags
