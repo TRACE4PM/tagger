9df72878e74862ef381963046e2d400f9c78def4
nalcime <nannito.alcime@etudiant.univ-lr.fr>
2023-06-15 17:18:41 +0200

    Update in utils.py and tar_rules.py

--- src/tagging/utils.py
+++ src/tagging/utils.py
@@ -1,10 +1,10 @@
 import json
+from fastapi.encoders import jsonable_encoder
 from .models.tag_rules import Tag_Rules_Model
 
 async def validate_json_content(file_content):
     tags_config = json.loads(file_content)
     tags = []
     for tag_config in tags_config:
-        #tags.append(Tag_Rules_Model(**tag_config))
-        tags.append(tag_config)
-    return tags
+        tags.append(Tag_Rules_Model(**tag_config))
+    return jsonable_encoder(tags)
