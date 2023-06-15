e7657fc3aba7f50a13dbe84bedaa69cdeee00161
nalcime <nannito.alcime@etudiant.univ-lr.fr>
2023-06-15 16:15:19 +0200

    Replace list by [] in utils.py

--- src/tagging/utils.py
+++ src/tagging/utils.py
@@ -3,7 +3,7 @@ from .models.tag_rules import Tag_Rules_Model
 
 async def validate_json_content(file_content):
     tags_config = json.loads(file_content)
-    tags = list()
+    tags = []
     for tag_config in tags_config:
         tags.append(Tag_Rules_Model(**tag_config))
     return tags
